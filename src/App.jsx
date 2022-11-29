241c559e1a3905a6d15ffee241697ba0eb035adc
martinbrumana <martinbrumana@gmail.com>
2022-11-29 18:08:17 -0300

    Ajustes v2

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,16 @@ import cataratas from "./assets/imgTask/cataratas.webp";
 import caminito from "./assets/imgTask/fb-caminito.jpg";
 import sietecolores from "./assets/imgTask/sietecolores.png";
 import usuahia from "./assets/imgTask/usuahia.jpeg";
+import mate from "./assets/imgTask/mate.jpg";
 
 function App() {
   let items = [
     { name: "Visitar Bariloche", id: 4, img: bariloche },
     { name: "Foto en las Cataratas de Iguazu", id: 1, img: cataratas },
+    { name: "Probar el mate", id: 3, img: mate },
     { name: "Bailar en Caminito, la boca", id: 2, img: caminito },
     { name: "Visitar Usuahia", id: 3, img: usuahia },
     { name: "Ver el Cerro de los siete colores", id: 5, img: sietecolores },
-    { name: "Orange", id: 3 },
     { name: "Apple", id: 1 },
     { name: "Banana", id: 2 },
     { name: "Orange", id: 3 },
@@ -60,7 +61,7 @@ function App() {
       </header>
 
       <h1>
-        100 Cosas que hacer: <strong>Argentina</strong>
+        100 Cosas que hacer: <span>Argentina</span>
       </h1>
 
       <div className="grid-container grid-container--fill">
